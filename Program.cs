d690068c4b3688347865709c4730f65b611f7c31
Emily <emily.reed0913@gmail.com>
2021-11-17 14:03:31 -0600

    almost finished

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company myCompany = new Company()
+            {
+                Name = "Planet Fitness"
+            };
+
+            
+            // Create three employees
+            Employee guy = new Employee()
+            {
+              FirstName = "Guy",
+              LastName = "Fiari",
+              Title = "Food Eater",
+              StartDate = new DateTime(2021,11,17)  
+            };
+            // Assign the employees to the company
+            myCompany.Employees.Add(guy);
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
